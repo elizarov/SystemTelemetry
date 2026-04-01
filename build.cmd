b6f1f1b054947364a4e61aa2d7152ff03941c7d8
Roman Elizarov <elizarov@gmail.com>
2026-04-01 23:47:36 +0300

    Fixed build so it works in sandbox

--- build.cmd
+++ build.cmd
@@ -5,8 +5,13 @@ call "%~dp0devenv.cmd"
 if errorlevel 1 exit /b %errorlevel%
 
 if not exist "%~dp0build" mkdir "%~dp0build"
-if not exist "%~dp0build\tmp" mkdir "%~dp0build\tmp"
-set "BUILD_TMP=%~dp0build\tmp\run-%RANDOM%-%RANDOM%"
+set "SCRATCH_ROOT=%TEMP%"
+if not defined SCRATCH_ROOT set "SCRATCH_ROOT=%TMP%"
+if not defined SCRATCH_ROOT if defined LOCALAPPDATA set "SCRATCH_ROOT=%LOCALAPPDATA%\Temp"
+if not defined SCRATCH_ROOT set "SCRATCH_ROOT=%~dp0build\tmp"
+set "SCRATCH_ROOT=%SCRATCH_ROOT%\SystemTelemetry"
+if not exist "%SCRATCH_ROOT%" mkdir "%SCRATCH_ROOT%"
+set "BUILD_TMP=%SCRATCH_ROOT%\run-%RANDOM%-%RANDOM%"
 if exist "%BUILD_TMP%" rmdir /s /q "%BUILD_TMP%" >nul 2>nul
 mkdir "%BUILD_TMP%"
 set "TMP=%BUILD_TMP%"
