ea8a01d743216f75668c196f41b527097f84eb45
Roman Elizarov <elizarov@gmail.com>
2026-04-09 23:41:56 +0300

    Refactored layout selection into config resolution

--- src/config_resolution.cpp
+++ src/config_resolution.cpp
@@ -120,3 +120,18 @@ bool SelectResolvedLayout(AppConfig& config, const std::string& requestedName) {
     config.layout.structure.cardsLayout = selected->cardsLayout;
     return true;
 }
+
+bool SelectLayout(AppConfig& config, const std::string& name) {
+    if (name.empty()) {
+        return false;
+    }
+    for (const auto& layout : config.layouts) {
+        if (layout.name == name) {
+            config.display.layout = layout.name;
+            config.layout.structure.window = layout.window;
+            config.layout.structure.cardsLayout = layout.cardsLayout;
+            return true;
+        }
+    }
+    return false;
+}
